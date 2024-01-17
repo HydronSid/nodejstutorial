354df7ff886bf6c33e0cbe7193c5cf9e4f1c537a
HydronSid <suvisiddhant6101999@gmail.com>
2024-01-17 18:03:43 +0530

    learned async with fs

---
+++ fs/Synchronus/fs_example_two.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+// Create a folder
+ fs.mkdirSync("Siddhant");
+
+// for creating file in folder
+ fs.writeFileSync('Siddhant/bio.txt',"My Name is Siddhant");
+
+// for appending data
+ fs.appendFileSync("Siddhant/bio.txt"," Hello !!");
+
+// for getting data without buffer data
+ var buf_data = fs.readFileSync("Siddhant/bio.txt","utf-8");
+ console.log(buf_data);
+
+// for rename file
+ fs.renameSync("Siddhant/bio.txt","Siddhant/myBio.txt");
+
+// for deleting file
+ fs.unlinkSync("Siddhant/myBio.txt");
+
+// for deleting Folder
+ fs.rmdirSync("Siddhant");
+
