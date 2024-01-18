26122395726ab71780d42f682615366a6044dde8
HydronSid <suvisiddhant6101999@gmail.com>
2024-01-18 11:36:51 +0530

    learned import export from other file

---
+++ importExport/index.js
@@ -0,0 +1,7 @@
+const {name ,add , sub , mult , div} = require('./oper')
+
+console.log(name);
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mult(5,5));
+console.log(div(5,5));
---
+++ importExport/oper.js
@@ -0,0 +1,18 @@
+
+const add = (a , b) => {
+    return a + b;
+}
+
+const sub = (a , b) => {
+    return a - b;
+}
+const mult = (a , b) => {
+    return a * b;
+}
+const div = (a , b) => {
+    return a / b;
+}
+
+const name = "Siddhant";
+
+module.exports = {name ,add , sub , mult ,div};
