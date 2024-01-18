82df4bbda150e3e7e99ee1e541cf3b07df2f396f
HydronSid <suvisiddhant6101999@gmail.com>
2024-01-18 14:18:20 +0530

    created server part 2

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,10 +1,19 @@
 const http = require('http'); 
-
+const log = console.log;
 const server = http.createServer((req , res)=> {
-    res.end('Hello response');
+    // console.log(req.url);
+    if(req.url == "/"){
+        res.end('Home Page');
+    }else if(req.url == "/about") {
+        res.end('About Page');
+    } else{
+        res.writeHead(404,{"Content-type":"text/html"});
+        res.end('<h1> 404 Not Found.</h1>');
+    }
+    
 });
 
 
 server.listen(8000 , "127.0.0.1",()=> {
-    console.log("listning to port no 8000");
+    log("listning to port no 8000");
 })
