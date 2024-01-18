c5cd0029409a5461d84dc9e0de636b1c8142c9db
HydronSid <suvisiddhant6101999@gmail.com>
2024-01-18 10:53:58 +0530

    os module

---
+++ osModule/osmod.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+console.log(`Architecture : ${os.arch()}`);
+console.log(`Free Memory : ${os.freemem()/1024/1024/1024}`);
+// console.log(os.cpus());
+console.log(`Home Directory : ${os.homedir()}`);
+console.log(`Host Name : ${os.hostname()}`);
+console.log(`Machine : ${os.machine()}`);
+console.log(`Platform : ${os.platform()}`);
+console.log(`Release : ${os.release()}`);
+console.log(`Temp Directory : ${os.tmpdir()}`);
+console.log(`Total Memory : ${os.totalmem()}`);
+console.log(`OS Type : ${os.type()}`);
+console.log(`OS Type : ${os.userInfo()}`);
