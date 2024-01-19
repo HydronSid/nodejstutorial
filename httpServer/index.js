60efb9c2c58285f7157fadb6205062f945c2defa
HydronSid <suvisiddhant6101999@gmail.com>
2024-01-19 17:49:47 +0530

    created node project

--- httpServer/index.js
+++ httpServer/index.js
@@ -3,19 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const log = console.log;
 
-const replaceTemplate = (temp , product)=> {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+const replaceTemplate = require('./userApi/helper/replaceTemplate');
 
 var tempOverview = fs.readFileSync(`${__dirname}/userApi/templates/template-overview.html`,"utf8");
 var tempCard = fs.readFileSync(`${__dirname}/userApi/templates/template-card.html`,"utf8");
@@ -26,8 +14,7 @@ var dataObj = JSON.parse(data);
 
 const server = http.createServer((req , res)=> {
     
-    const  {query , pathname} =  url.parse(req.url, true);
-
+    const  {query , pathname} =  url.parse(req.url, true);;
     // Overview Page
     if(pathname == "/" || pathname == "/overview"){
         res.writeHead(200,{"Content-type":"text/html"});
